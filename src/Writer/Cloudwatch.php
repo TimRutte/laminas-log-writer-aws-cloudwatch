88e14fa95066090ae818da5be0bc83668a59b4de
Tim Rutte <info@timrutte.de>
2023-02-24 21:06:46 +0100

    added aws config validator

--- src/Writer/Cloudwatch.php
+++ src/Writer/Cloudwatch.php
@@ -3,51 +3,94 @@
 namespace TimRutte\Laminas\Log\Writer;
 
 use Aws\CloudWatchLogs\CloudWatchLogsClient;
+use Laminas\Log\Exception\InvalidArgumentException;
 use Laminas\Log\Writer\AbstractWriter;
 
 class Cloudwatch extends AbstractWriter
 {
     /** @var string $awsKey AWS access key */
-    private $awsKey;
+    private string $awsKey;
 
     /** @var string $awsSecret AWS secret key */
-    private $awsSecret;
+    private string $awsSecret;
 
     /** @var string $awsRegion AWS region */
-    private $awsRegion;
+    private string $awsRegion;
 
     /** @var string $groupName Cloudwatch group name */
-    private $groupName;
+    private string $groupName;
 
     /** @var string $streamName Cloudwatch stream name */
-    private $streamName;
+    private string $streamName;
 
+    /** @var integer $retentionInDays Log retention in days */
+    private int $retentionInDays;
+    
     /** @var string $version AWS API version */
-    private $version;
+    private string $version;
 
     /**
      * Set AWS credentials and Cloudwatch settings
      *
-     * @param $awsKey
-     * @param $awsSecret
-     * @param $awsRegion
-     * @param $groupName
-     * @param $streamName
-     * @param $version
+     * @param string $awsKey
+     * @param string $awsSecret
+     * @param string $awsRegion
+     * @param string $groupName
+     * @param string $streamName
+     * @param int $retentionInDays
+     * @param string $version
      * @return void
      */
-    public function configureAws($awsKey, $awsSecret, $awsRegion, $groupName, $streamName, $version = 'latest')
+    public function configureAws(
+        string $awsKey, 
+        string $awsSecret, 
+        string $awsRegion, 
+        string $groupName, 
+        string $streamName, 
+        int $retentionInDays = 14, 
+        string $version = 'latest'
+    ): void
     {
         $this->awsKey = $awsKey;
         $this->awsSecret = $awsSecret;
         $this->awsRegion = $awsRegion;
         $this->groupName = $groupName;
         $this->streamName = $streamName;
+        $this->retentionInDays = $retentionInDays;
         $this->version = $version;
     }
 
+    private function validateAwsConfiguration()
+    {
+        if (empty($this->awsKey)) {
+            throw new InvalidArgumentException('Missing AWS access key');
+        }
+
+        if (empty($this->awsSecret)) {
+            throw new InvalidArgumentException('Missing AWS secret key');
+        }
+
+        if (empty($this->awsRegion)) {
+            throw new InvalidArgumentException('Missing AWS region');
+        }
+
+        if (empty($this->groupName)) {
+            throw new InvalidArgumentException('Missing cloudwatch group name');
+        }
+
+        if (empty($this->streamName)) {
+            throw new InvalidArgumentException('Missing cloudwatch stream name');
+        }
+
+        if (!is_numeric($this->retentionInDays)) {
+            throw new InvalidArgumentException('Unknown retention. Integer is required.');
+        }
+    }
+    
     public function doWrite(array $event)
     {
+        $this->validateAwsConfiguration();
+        
         $timestamp = time();
 
         $cloudWatchClient = new CloudWatchLogsClient(
@@ -55,7 +98,7 @@ class Cloudwatch extends AbstractWriter
                 'version' => $this->version,
                 'region' => $this->awsRegion,
                 'stream_name' => $this->streamName,
-                'retention' => 14, // in days
+                'retention' => $this->retentionInDays,
                 'group_name' => $this->groupName,
                 'credentials' => [
                     'key' => $this->awsKey,
